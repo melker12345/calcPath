a0bf126e0177d3010fd22deb15638dff30ba772b
Melker <memleoberg@gmail.com>
2026-06-01 00:06:31 +0200

    refactor(modules): start Linear Algebra decomposition
    - Created src/lib/modules/linear-algebra/ structure
    - Extracted first topic: vectors
    - Wired vectorsModule into linalg-modules.ts
    - Build remains green

---
+++ src/lib/modules/linear-algebra/index.ts
@@ -0,0 +1,32 @@
+/**
+ * Linear Algebra module content
+ *
+ * This barrel will export the combined list of all linear algebra modules
+ * after we finish splitting them out of the original monolithic linalg-modules.ts.
+ *
+ * Following the same pattern established for Calculus.
+ */
+
+import type { ModuleContent } from "../types";
+
+// Re-export types for convenience
+export type { ModuleContent, ModuleSection, WorkedExample } from "../types";
+
+// Extracted topics
+export { vectorsModule } from "./vectors";
+// export { matricesModule } from "./matrices";           // TODO
+// export { systemsModule } from "./systems";             // TODO
+// export { spacesModule } from "./spaces";               // TODO
+// export { eigenvaluesModule } from "./eigenvalues";     // TODO
+// export { determinantsModule } from "./determinants";   // TODO
+// export { orthogonalityModule } from "./orthogonality"; // TODO
+// export { symmetricMatricesModule } from "./symmetric-matrices"; // TODO
+
+// When complete, this will become something like:
+// export const linearAlgebraModules: ModuleContent[] = [
+//   vectorsModule,
+//   matricesModule,
+//   ...
+// ];
+
+export const linearAlgebraModules: ModuleContent[] = []; // Will be populated during migration
